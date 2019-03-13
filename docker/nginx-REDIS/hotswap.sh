c65137f509d9f24e60c1cf00fd30dd6779534b69
Niklas Kraemer <nkraemer97@gmail.com>
2019-03-13 00:21:59 +0000

    implemented hotswap bash script

---
+++ docker/nginx-REDIS/hotswap.sh
@@ -0,0 +1,17 @@
+#/!bin/bash
+PARAM=$1
+FILE=test.conf
+PREV=$(awk '/    server/{print $2}' $FILE)
+
+if [ "$PARAM:6379;" = $PREV ]
+then
+	echo "Warning: currently already using that ip"  
+else
+	sed -i "s/^\(    server\)\(.*\)$/\1 $PARAM:6379;/" $FILE
+	#need to run reload, possibly just the 
+	/usr/sbin/nginx -s reload
+fi
+
+#mac version, regex should be the same
+#sed -i "" "s/^\( *server\)\(.*\)$/\1 $PARAM/" $FILE
+
